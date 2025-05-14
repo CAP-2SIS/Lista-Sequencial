d5af61398b1b3397a9cadc33a4246d553e7cd6c9
RafaelVi <rafaelalmeida383@gmail.com>
2025-05-14 20:41:40 -0300

    Adicao fila int e metodo insere

---
+++ src/ListaInt.java
@@ -0,0 +1,51 @@
+public class ListaInt {
+    static No lista = null;
+
+    private class No {
+        int dado;
+        No prox;
+    }
+
+    public boolean isEmpty() {
+        return lista == null;
+    }
+
+    public void insere(int dado) {
+        No novo = new No();
+        novo.dado = dado;
+
+        if (isEmpty()) {
+            lista = novo;
+            lista.prox = null;
+        } else if (dado < lista.dado) {
+            novo.prox = lista;
+            lista = novo;
+        } else {
+            No aux = lista;
+
+            while (aux.prox != null) {
+                if (aux.prox.dado < novo.dado) {
+                    aux.prox = aux;
+                    break;
+                }
+            }
+            novo.prox = aux.prox;
+            aux.prox = novo;
+        }
+    }
+
+
+public void print() {
+    StringBuilder printer = new StringBuilder();
+    printer.append("[");
+    No aux = lista;
+    while (aux.prox != null) {
+        printer.append(aux.dado).append(", ");
+        aux = aux.prox;
+    }
+    printer.append(aux.dado).append("]");
+    System.out.println(printer);
+
+}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ListaInt listaInt = new ListaInt();
+        listaInt.insere(3);
+        listaInt.insere(2);
+        listaInt.insere(1);
+        listaInt.print();
+    }
+}
